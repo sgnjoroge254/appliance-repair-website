ce053a09e37a23a360c180ba42979aa60cadfa12
Stephen Njoroge <your-github-email@example.com>
2026-09-11 12:49:23 +0300

    Add missing Header to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${archivo.variable} ${plexSans.variable} ${plexMono.variable} font-body`} suppressHydrationWarning>
+        <Header />
         <main>{children}</main>
         <Footer />
       </body>
